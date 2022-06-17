e4c2f3a3d2903ee5b45bd30c4a1ac4dcc9542884
Jiun <jiun48562@gmail.com>
2022-06-18 01:45:51 +0900

    [FIX] ajax address

--- public/javascripts/map/mapLoader.js
+++ public/javascripts/map/mapLoader.js
@@ -73,7 +73,7 @@ kakao.maps.event.addListener(map, 'dragend', function() {
     neLng = currentBounds.getNorthEast().getLng();
 
     const inputdata = {'swLat' : swLat, 'swLng' : swLng, 'neLat' : neLat, 'neLng' : neLng};
-    sendBoundAjax('http://localhost:3000/setbound-ajax', inputdata)
+    sendBoundAjax('/setbound-ajax', inputdata)
 });
 
 // 지도가 이동, 확대, 축소로 인해 지도영역이 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
@@ -91,10 +91,10 @@ kakao.maps.event.addListener(map, 'zoom_changed', function() {
     neLng = currentBounds.getNorthEast().getLng();
 
     const inputdata = {'swLat' : swLat, 'swLng' : swLng, 'neLat' : neLat, 'neLng' : neLng};
-    sendBoundAjax('http://localhost:3000/setbound-ajax', inputdata)
+    sendBoundAjax('/setbound-ajax', inputdata)
 });
 
-//send함수 'http://localhost:3000/setbound-ajax'주소에 inputdata를 보냅니다
+//send함수 '/setbound-ajax'주소에 inputdata를 보냅니다
 function sendBoundAjax(url, data) {
 
     let ajaxData = data;
